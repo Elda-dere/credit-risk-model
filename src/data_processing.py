411338904514b388b3565f92aadf10fad726003c
Elda-dere <Elda-dere@users.noreply.github.com>
2026-06-04 12:27:54 -0700

    Add complete Tasks 1 & 2: EDA notebook, README, data_processing

---
+++ src/data_processing.py
@@ -0,0 +1,15 @@
+﻿import pandas as pd
+
+def load_raw_data(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+def create_rfm_features(df):
+    df['TransactionStartTime'] = pd.to_datetime(df['TransactionStartTime'])
+    max_date = df['TransactionStartTime'].max()
+    rfm = df.groupby('CustomerId').agg({
+        'TransactionStartTime': lambda x: (max_date - x.max()).days,
+        'TransactionId': 'count',
+        'Amount': lambda x: x[x > 0].sum()
+    })
+    return rfm
